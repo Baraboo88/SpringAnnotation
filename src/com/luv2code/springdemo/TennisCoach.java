4163be38c2fd43e3b4307c73a207815bb5717f5c
Baraboo88 <6990413@mail.ru>
2019-01-20 19:13:43 +0300

    With Random Fortune

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,23 +2,30 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
-    @Autowired
-    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
+    @Value("${foo.email}")
+    private String email;
+    @Value("${foo.team}")
+    private String team;
 
+    @Autowired
 
-    public TennisCoach() {
+    public TennisCoach(@Qualifier("randomFortuneService") FortuneService fortuneService) {
         System.out.println(">> TennisCoach: inside default contstructor");
+        this.fortuneService = fortuneService;
+
     }
 
     @Override
     public String getDailyWorkout() {
-
+        System.out.println(email);
+        System.out.println(team);
         return "Practice your backhand volley";
     }
 
